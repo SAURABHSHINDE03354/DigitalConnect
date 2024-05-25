c548fddbc1ac486dac0df6c9b8ec3371c36f1d5a
SAURABHSHINDE03354 <saurabhshinde03354@gamil.com>
2024-05-25 15:34:02 +0530

    NavBar color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -277,7 +277,7 @@ const NavBar = () => {
   };
 
   return (
-    <div className="bg-base-200 dark:bg-slate-900 dark:text-white fixed top-0 left-0 right-0 z-50 shadow-md">
+    <div className="bg-base-200 bg-slate-900 text-white fixed top-0 left-0 right-0 z-50 shadow-md">
       <div className="max-w-screen-2xl container mx-auto md:px-5 px-3 flex justify-between items-center py-4">
         <Link to="/" className="text-2xl md:text-3xl font-bold cursor-pointer">
           DigitalConnect
